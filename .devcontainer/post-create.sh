0ce147e7e3841ea588045287d91d36baf1cbe0dc
Tofail Ahammed <tushar583258@gmail.com>
2026-07-19 19:06:00 +0000

    Professional DevContainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "======================================="
+echo " BugBountyOS Bootstrap"
+echo "======================================="
+
+chmod +x install.sh
+
+bash install.sh
